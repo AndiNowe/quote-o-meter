e40e9db25acccb39ccbf964397c9a91a1cdf3f8c
Andi <andrea.bonet01@gmail.com>
2021-02-22 20:56:13 +0100

    Comments in GamesView.js and GamesView.css

--- client/src/components/GamesView.js
+++ client/src/components/GamesView.js
@@ -5,14 +5,16 @@ import "./GamesView.css";
 
 function GamesView(props) {
 
+  //This prop displays all the games from the table "games" on one page. Also the button to acces GameForm.
 
   let [games, setGames] = useState([]);
+
+  //This is the useHistory to be able to go back to the previous page 
   const history = useHistory();
-  
+
+
+  //This fetches the table games from the database as soon as this view is loaded.
   useEffect(() => {
-    /**
-     * Using .then/.catch in useEffect()
-     **/
 
     fetch('/games')
         .then(result => result.json())
@@ -25,16 +27,21 @@ function GamesView(props) {
   }, []);
 
 
-  
+  //Function to delete a game
   function deleteGame(event) {
 
+    //The game info is displayed on a card. This card has a delete button, which used to re-direct "inside" of the game page, as well as deleting it
+    //causing an error. This is due to the children click (delete button) propagating to the parent (card), and this bit of code stops it.
+    //To delete the game you press the button, to enter the game, you press the card or the "Load Game" button. 
     event.stopPropagation();
 
+    //Delete on the database
     let options = {
       method: "DELETE",
       body: JSON.stringify(games)
     };
 
+    //fetch the new list after waiting for it to update. 
     fetch(`/games/${event.clickedGame.id}`, options)
       .then(result => result.json())
       .then(games => {
@@ -47,8 +54,13 @@ function GamesView(props) {
 
 
 
+  //This function sends the object game as props up to Routes, so when we change view, we can only see the characters and quotes associated
+  //to this particular game id.
+
   function handleClick (g) {
     props.getGame(g);
+
+    //When we click the card or the "Load Game" button, we go to this route, which takes the specific id of the game we click onto the url
     history.push(`/quoteandchar/${g.id}`);
     
   }
@@ -68,29 +80,30 @@ function GamesView(props) {
           </Link>
         </div>
 
+        {/*a couple line jumps, some space :D breathe */}
         <br></br>
         <br></br>
 
         <h3 className= "Title">Saved Games</h3>
 
         {/* ".map()" won't render until "games" has value */}
-
-        
-
         <div className="games ">
           {games &&
                   games.map(g => (
-                    // <Link to={`/quoteandchar/${g.id}`}>
+                    
                       <div 
                         key={g.id}
                         onClick={() =>handleClick(g)}
                         className = "card"
                       >
 
+                        {/*This is the info displayed on the card*/}
                         <ul>Name: {" "+g.name}</ul> 
                         <ul>Universe: {" "+g.universe}</ul>
+                        {/*This bit creates a new object of the Date class, to set it with the right format to then convert it to string, for display*/}
                         <ul>Date: {" "+(new Date(g.date)).toLocaleDateString()}</ul>
 
+                        {/*This button links to that specific URL when click, and also triggers handleClick to send props*/}
                         <Link to={`/quoteandchar/${g.id}`}>
                           <button  
                             className = "button"
@@ -102,6 +115,7 @@ function GamesView(props) {
                           </button> 
                         </Link>
 
+                        {/*With this button I don't only send the object g as props, also event, to be able to stop the propagation*/}
                         <button  
                           className = "button"
                           type="button"
@@ -116,15 +130,11 @@ function GamesView(props) {
 
 
                       </div>
-                      // </Link>
+                      
                       )
                   )
               }   
         </div>
-
-        
-        
-
 
       </div>
       
