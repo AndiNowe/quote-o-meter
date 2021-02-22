cc8db5581d724ff20cc4ad579dfe79ec3c115899
Andi <andrea.bonet01@gmail.com>
2021-02-22 16:56:19 +0100

    Comments in characters.js

--- routes/characters.js
+++ routes/characters.js
@@ -16,12 +16,14 @@ router.use(bodyParser.json());
 /*******************Routes**************** */
 
 
-//Get homepage
+//Get homepage, no need to use this. Used it as the default when there were no other queries. 
 
 // router.get("/", (req, res) => {
 // //   Once connected, expects a response with a message
 //   res.send({ message: "Welcome to the QUOTE- O'BRIAN, CHARMANDER" });
 // });
+
+
 
 //GET all data from games table
 router.get("/", async (req, res) => {
@@ -31,13 +33,14 @@ router.get("/", async (req, res) => {
     let results = await db("SELECT * FROM characters ORDER BY id ASC;");
 
     if (results.data.length) {
-    //check
-    console.log("RESULTS", results);
-    //send back the full list of items with status
-    res.status(200).send(results.data); 
+      //check
+      //console.log("RESULTS", results);
+
+      //send back the full list of items with status
+      res.status(200).send(results.data); 
 
     } else { 
-    res.status(404).send({ error: "Db is inaccesible or empty." });
+     res.status(404).send({ error: "Db is inaccesible or empty." });
     }
 
     //Catch any errors
@@ -47,6 +50,7 @@ router.get("/", async (req, res) => {
   }
   
 });
+
 
 //GET data by ID
 router.get("/:id", async (req, res) => {
@@ -62,7 +66,7 @@ router.get("/:id", async (req, res) => {
   try {
 
     let results = await db(sql);
-    //returns obj and not the array for items, results.data is array, we want the index of [0]
+    //resulta.data is the whole object, we want the value of [0]
     res.send(results.data[0]);
 
   } catch (err) {
@@ -72,6 +76,7 @@ router.get("/:id", async (req, res) => {
 
 
 //GET data by Game ID
+//To get a character while fetching by foreign key "game_id"
 router.get("/game_id/:id", async (req, res) => {
   
   let id = req.params.id;
@@ -92,9 +97,11 @@ router.get("/game_id/:id", async (req, res) => {
   }
 });
 
-// POST a game
+
+// POST a character
 router.post("/", async (req, res) => {
-  
+
+  //we will never write games_id on the user UI, we will always get it as props, but it always must be there to post a character
   let { player, charname, race, charclass, description, games_id  } = req.body;
   
   let sql = `
@@ -116,7 +123,7 @@ router.post("/", async (req, res) => {
 });
 
 
-//DELETE data by ID
+//DELETE character by ID
 router.delete("/:id", async (req, res) => {
 
   let id = req.params.id;
