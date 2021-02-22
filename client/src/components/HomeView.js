5d479849255531af5215e02a43addd6cbe51548c
Andi <andrea.bonet01@gmail.com>
2021-02-22 21:22:22 +0100

    Comments in HomeView.js

--- client/src/components/HomeView.js
+++ client/src/components/HomeView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+/*This is from an icon package called Fort Awesome */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDiceD20 } from '@fortawesome/free-solid-svg-icons'
 import './HomeView.css';
@@ -10,14 +11,13 @@ function Home() {
 
 
 
-
+  //we will need this to go to the view /games when we click the icon.
   const history = useHistory();
 
     return (
       <div className="Home">
-
-
       
+      {/*This is the icon's code */}
       <div  className="RollButton" > 
         
         <FontAwesomeIcon 
@@ -31,7 +31,7 @@ function Home() {
 
       </div>
       
-  
+   
       </div>
     );
   }
