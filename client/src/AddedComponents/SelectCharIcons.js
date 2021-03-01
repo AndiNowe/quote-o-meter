dca56fcf16d09136cf3689052d23ce80a2bbcd8b
Donna Chin <donnchin@gmail.com>
2021-03-01 16:28:50 -0500

    Renamed all character icons in SelectCharIcons folder

---
+++ client/src/AddedComponents/SelectCharIcons.js
@@ -0,0 +1,27 @@
+import Samurai from '../components/ClientImages/Samurai.png'
+import Berzerker from '../components/ClientImages/Berzerker.png'
+import Mage from '../components/ClientImages/Mage.png'
+
+export const SelectCharIcons = [
+    {
+        image: {Samurai}
+    },
+    {
+        image: {Berzerker}
+    },
+    {
+        image: {Mage}
+    },
+    {
+        image: {}
+    },
+    {
+        image: {}
+    },
+    {
+        image: {}
+    }
+
+
+
+]
